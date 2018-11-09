abb87a0de3e730859822bacb012d42fa02e291d7
KyleAres <kylealtimore@gmail.com>
2018-11-09 09:22:47 +0000

    gay

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const {google} = require('googleapis');
 fs.readFile('config.json', (err, content) => {
 	if (err) return console.log('Error loading client secret file:', err);
 	// Authorize a client with credentials, then call the Google Sheets API.
-	authorize(JSON.parse(content), listMajors);
+	authorize(JSON.parse(content), spreadsheetCommand);
 });
 
 /**
