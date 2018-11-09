6637f68739438aefd0970b08bf000902dfc2628c
KyleAres <kylealtimore@gmail.com>
2018-11-09 09:19:59 +0000

    gay

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const readline = require('linebyline');
 const {google} = require('googleapis');
 
 // Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
+fs.readFile('config.json', (err, content) => {
 	if (err) return console.log('Error loading client secret file:', err);
 	// Authorize a client with credentials, then call the Google Sheets API.
 	authorize(JSON.parse(content), listMajors);
