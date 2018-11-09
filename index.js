fd5168062ef7d0c142344dd431b4d20cd2f6e872
KyleAres <kylealtimore@gmail.com>
2018-11-09 09:08:51 +0000

    gay

--- index.js
+++ index.js
@@ -5,10 +5,65 @@ const fs = require('fs');
 const readline = require('linebyline');
 const {google} = require('googleapis');
 
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+	if (err) return console.log('Error loading client secret file:', err);
+	// Authorize a client with credentials, then call the Google Sheets API.
+	authorize(JSON.parse(content), listMajors);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+	const {client_secret, client_id, redirect_uris} = credentials.installed;
+	const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+// Check if we have previously stored a token.
+	fs.readFile(TOKEN_PATH, (err, token) => {
+		if (err) return getNewToken(oAuth2Client, callback);
+		oAuth2Client.setCredentials(JSON.parse(token));
+		callback(oAuth2Client);
+	});
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+	const authUrl = oAuth2Client.generateAuthUrl({
+		access_type: 'offline',
+		scope: SCOPES,
+	});
+	console.log('Authorize this app by visiting this url:', authUrl);
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+
+	rl.question('Enter the code from that page here: ', (code) => {
+		rl.close();
+		oAuth2Client.getToken(code, (err, token) => {
+			if (err) return console.error('Error while trying to retrieve access token', err);
+			oAuth2Client.setCredentials(token);
+			// Store the token to disk for later program executions
+			fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+				if (err) console.error(err);
+				console.log('Token stored to', TOKEN_PATH);
+			});
+			callback(oAuth2Client);
+		});
+	});
+}
+
 client.on("ready", () => console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`));
-// Example of changing the bot's playing game to something useful. `client.user` is what the
-// docs refer to as the "ClientUser".
-//client.user.setActivity(`Serving ${client.guilds.size} servers`);
+client.user.setActivity(`Serving ${client.guilds.size} servers`);
 
 // checks command and calls proper function
 client.on("message", (message) => {
@@ -29,15 +84,7 @@ client.on("message", (message) => {
         processCommand(message);
     }
 })
-/*
-	THIS IS SAME AS ABOVE
-	// Prevent bot from responding to its own messages
-	if(message.author.bot) return;
 
-	if(message.content.indexOf(prefix) !== 0) return;
-	//  [same as above] would check if it's not prefix then ignore (should be first if used and break out of bot)
-	//if(!message.content.startsWith(prefix)) return;
- */
 //start function - to be split between functions
 function processCommand(message) {
 	// reply
