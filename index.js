05666bef42d9b0ee0698199a0745c3bc26a50384
KyleAres <kylealtimore@gmail.com>
2018-11-09 09:10:05 +0000

    gay

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function getNewToken(oAuth2Client, callback) {
 }
 
 client.on("ready", () => console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`));
-client.user.setActivity(`Serving ${client.guilds.size} servers`);
+//client.user.setActivity(`Serving ${client.guilds.size} servers`);
 
 // checks command and calls proper function
 client.on("message", (message) => {
