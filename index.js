a62e8d4d38bae6b8f98335ea50a1faab6f245135
KyleAres <kylealtimore@gmail.com>
2018-11-15 23:54:53 +0000

    gay

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ fs.readFile('config.json', (err, content) => {
 function authorize(credentials, callback) {
 	//const {client_secret, client_id, redirect_uris} = credentials.installed;
 	const oAuth2Client = new google.auth.OAuth2(client_id, client_secret); //redirect_uris[0]
+}
 /*
 // Check if we have previously stored a token.
 	fs.readFile(token, (err, token) => {
